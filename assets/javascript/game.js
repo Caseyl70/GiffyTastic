2f7571442223f28ca08d7d898577801027933639
Zach Christie <zachchristieofficial@gmail.com>
2019-09-19 06:37:20 -0600

    Adding pseudo code to the javascript page and for loop/Query URL

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+//Need to add an on.click function for the button menu
+$("#buttons").on("click", "buttons", function() {
+    //Grab my Variable and Query URL and plug it in
+    var keyword = $(this).attr("#data-world");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=1UDXeUjPXD7dVqzhJqMItp1MDQrx5uth&limit=10";
+    console.log(keyword);
+    
+    //.AJAX method to grab the URL and method: GET 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+console.log(response)
+        var results = response.data;
+        for (var i = 0; i < ratings.length; i++) {
+            var worldDiv = $("<div>");
+
+            var ratings = results[i].ratings;
+            var p = $("<p>").text("Rating: " + ratings);
+            var worldImage = $("<img>")
+
+
+        }
+    }
+}
+
+
+//Create a results variable, and add a for loop so it runs each time when you click a button
+//Will need to use my new div to set equal to what will pull images(still, animated)
+//And then prepend the new div to the page
